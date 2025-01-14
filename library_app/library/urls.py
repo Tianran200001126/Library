60aca7aa3ed7e78a004828670de4476f628578e0
Tianran200001126 <tianranwang20000@gmail.com>
2025-01-14 19:08:43 +0000

    implement assign a book to the reader

--- library_app/library/urls.py
+++ library_app/library/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('books/<uuid:pk>', views.book_detail, name='book_detail'),
     path('readers/', views.list_readers, name='list_readers'),  
-    path('readers/<uuid:pk>/', views.reader_detail, name='reader_detail')
+    path('readers/<uuid:pk>/', views.reader_detail, name='reader_detail'),
+    path('books/<uuid:book_pk>/assign/', views.assign_book, name='assign_book'),
 ]
--- library_app/library/views.py
+++ library_app/library/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render , get_object_or_404
+from django.shortcuts import redirect
 from .models import Book , Reader 
 
 
-# Create your views here.
-
 def list_books(request):
     books = Book.objects.all()
-    return render(request,'library/list_book.html',{'books':books})
+    readers = Reader.objects.all()
+    return render(request,'library/list_book.html',{'books':books,'readers':readers})
 
 
 def book_detail(request, pk):
@@ -26,3 +26,27 @@ def reader_detail(request, pk):
         'reader': reader,
         'borrowed_books': borrowed_books,
     })
+
+
+def assign_book(request,book_pk):
+    print(book_pk)
+    book = get_object_or_404(Book,id=book_pk)
+    if request.method == 'POST':
+        reader_id = request.POST.get('reader')
+
+        if not reader_id:
+            return HttpResponse("Error: The reader is not selected, Please assign a book to a reader", status=400)
+
+        try:
+            reader = get_object_or_404(Reader,id=reader_id)
+            book.loaned_to = reader
+            book.save()
+            return redirect('list_books')
+        except Reader.DoesNotExist:
+            return HttpResponse("Error: Reader not found in the database.", status=404)     
+
+
+    return redirect('list_books')           
+
+
+
